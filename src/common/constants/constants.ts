8a79d40bc01490bc92cc707eace31dfc284215fa
vadymShtefanika <vadym.shtefanika@assist.ro>
2022-11-26 18:21:02 +0200

    Finished login features.

--- src/common/constants/constants.ts
+++ src/common/constants/constants.ts
@@ -1,6 +1,9 @@
 export enum ALT_IMG {
+  APP_LOGO = 'Logo aplicatiei',
   LOGIN_BG = 'Autentificare imaginea reprezentativa',
 }
+
+export const UTILAJES_ADMIN_ACCESS_TOKEN = 'UTILAJES_ADMIN_ACCESS_TOKEN'
 
 export enum BUTTON_LABELS {
   LOGIN = 'Logare',
--- src/features/login/login.tsx
+++ src/features/login/login.tsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
+import { Navigate } from 'react-router-dom'
 
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from 'firebaseInit'
 import { FormikValues, useFormik } from 'formik'
 import * as Yup from 'yup'
 
+import logo from 'common/assets/logo.svg'
 import loginBg from 'features/login/assets/login-bg.svg'
 
 import { Button } from 'common/components/Button/Button'
-import { ALT_IMG, BUTTON_LABELS } from 'common/components/constants/constants'
 import { Input } from 'common/components/Input/Input'
+import { ALT_IMG, BUTTON_LABELS } from 'common/constants/constants'
+import { AuthService } from 'common/services/auth-service'
+
+import { PATHS } from 'layout/paths'
 
 import { LOGIN_LABELS } from 'features/login/constants/constants'
 
 import styles from './login.module.scss'
 
 export const Login = () => {
+  const [loginError, setLoginError] = useState<string | null>(null)
   const formik = useFormik({
     initialValues: {
       email: '',
@@ -25,12 +33,25 @@ export const Login = () => {
         .required('Adresa de email este obligatorie'),
       password: Yup.string().required('Parola este obligatorie'),
     }),
-    onSubmit: (values: FormikValues) => {
-      console.log(values)
+    onSubmit: async (values: FormikValues) => {
+      try {
+        const credentials = await signInWithEmailAndPassword(
+          auth,
+          values.email,
+          values.password,
+        )
+        AuthService.setToken(credentials.user.refreshToken)
+      } catch (e) {
+        let errorMessage = e.message.split('/')
+        errorMessage = errorMessage[errorMessage.length - 1]
+          .replace(').', '')
+          .replaceAll('-', ' ')
+        setLoginError(errorMessage)
+      }
     },
   })
 
-  console.log(formik)
+  if (AuthService.getToken()) return <Navigate to={PATHS.HOME} replace />
 
   return (
     <div className={styles.parent}>
@@ -43,6 +64,11 @@ export const Login = () => {
           />
         </span>
         <span className={styles.parentSecond}>
+          <img
+            className={styles.parentLogo}
+            src={logo}
+            alt={ALT_IMG.APP_LOGO}
+          />
           <h1 className={styles.parentTitle}>{LOGIN_LABELS.LOGIN}</h1>
           <form className={styles.parentForm} onSubmit={formik.handleSubmit}>
             <div className={styles.parentFormItem}>
@@ -53,7 +79,10 @@ export const Login = () => {
                 label='Email'
                 placeholder='Email'
                 value={formik.values.email}
-                onChange={formik.handleChange}
+                onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                  formik.handleChange(event)
+                  if (loginError) setLoginError(null)
+                }}
               />
               {formik.touched.email && formik.errors.email ? (
                 <p className={styles.parentErrorMessage}>
@@ -70,7 +99,10 @@ export const Login = () => {
                 label='Parola'
                 placeholder='Parola'
                 value={formik.values.password}
-                onChange={formik.handleChange}
+                onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                  formik.handleChange(event)
+                  if (loginError) setLoginError(null)
+                }}
               />
               {formik.touched.password && formik.errors.password ? (
                 <p className={styles.parentErrorMessage}>
@@ -78,6 +110,10 @@ export const Login = () => {
                 </p>
               ) : null}
             </div>
+
+            {loginError ? (
+              <p className={styles.parentLoginErrorMessage}>{loginError}</p>
+            ) : null}
 
             <div className={styles.parentSubmit}>
               <Button htmlType='submit' modifier='primary'>
